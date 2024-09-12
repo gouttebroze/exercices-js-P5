d0097d5e903f44e0a976d0a9772d7a8936db0bd6
Hugues, programmer fullstack <docteurfutur@gmail.com>
2024-09-12 21:01:18 +0200

    confirm all tests passed with screenshots

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,9 @@
-//votre code ici
+function printNumbers(n) { 
+  let print = "";
+  for (let i = 0; i < n; i++) {
+    print = print + (i === 0 ? "" : " ") + (i + 1).toString();  
+  }
+  return print;
+}
 
 export default printNumbers
--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,11 @@
-// votre code ici
+function checkAge(int) {
+  if (int < 18) {
+    return 'Vous êtes mineur.';
+  } else if (int >= 65) {
+    return 'Vous êtes senior.';
+  } else {
+    return 'Vous êtes majeur.';
+  }
+}
 
 export default checkAge;
